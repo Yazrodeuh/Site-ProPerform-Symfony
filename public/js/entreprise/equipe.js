768e6b859c811515f5034154d209ae79c630a640
maxime <maxou.pepette@gmail.com>
2021-10-21 15:08:31 +0200

    equipe + locaux

--- public/js/entreprise/equipe.js
+++ public/js/entreprise/equipe.js
@@ -28,42 +28,36 @@ function afficher_equipe_top(id){
  * @type {string[][]}
  */
 let tab_Personne = [
-    ["Docteur en chirurgie dentaire", "Ingénieur Ecole Centrale Marseille", "Master CAAE IAE Montpellier", "Fondateur de Sup'Perform", "18 ans d'expérience dans l'enseignement supérieur",],
-    ["Titulaire d’un Master II en droit de l’Université de Montpellier en 2015", "Titulaire du D.U Certificat d’Études Judiciaires", "Juriste formatrice au profit de différentes CCI et organismes de formation"],
-    [ "Docteur en droit de l’Université de Montpellier  en 2015", "Enseignant en Licence et Master \n à la Faculté de droit de Montpellier (2010-2017)", "Juriste formateur pour les collectivités territoriales", "10 ans d'expérience dans l’enseignement"]];
+    ["Anthony JULIA", "Docteur en chirurgie dentaire", "Ingénieur Ecole Centrale Marseille", "Master CAAE IAE Montpellier", "Fondateur de Sup'Perform", "18 ans d'expérience dans l'enseignement supérieur",],
+    ["Léa BRUET","Titulaire d’un Master II en droit de l’Université de Montpellier en 2015", "Titulaire du D.U Certificat d’Études Judiciaires", "Juriste formatrice au profit de différentes CCI et organismes de formation"],
+    ["Jonathan GARCIA", "Docteur en droit de l’Université de Montpellier  en 2015", "Enseignant en Licence et Master \n à la Faculté de droit de Montpellier (2010-2017)", "Juriste formateur pour les collectivités territoriales", "10 ans d'expérience dans l’enseignement"]];
 
 function eventModal(){
 
     anthony.addEventListener("mouseover", function (){
         creer_info_personne(0);
-        afficher_equipe_top("prenom_anthony");
         afficher_equipe_top("info_equipe");
     })
 
     anthony.addEventListener("mouseout", function (){
-        cacher_equipe_top("prenom_anthony");
         cacher_equipe_top("info_equipe");
     })
 
     lea.addEventListener("mouseover", function (){
         creer_info_personne(1);
-        afficher_equipe_top("prenom_lea");
         afficher_equipe_top("info_equipe");
     })
 
     lea.addEventListener("mouseout", function (){
-        cacher_equipe_top("prenom_lea");
         cacher_equipe_top("info_equipe");
     })
 
     jonathan.addEventListener("mouseover", function (){
         creer_info_personne(2);
-        afficher_equipe_top("prenom_jonathan");
         afficher_equipe_top("info_equipe");
     })
 
     jonathan.addEventListener("mouseout", function (){
-        cacher_equipe_top("prenom_jonathan");
         cacher_equipe_top("info_equipe");
     })
 
@@ -77,7 +71,13 @@ function creer_info_personne(numeroPersonne){
 
     emplacement_modal.firstElementChild.innerHTML = "";
 
-    for (let i = 0; i < tab_Personne[numeroPersonne].length; i++) {
+    let nom = document.createElement("div");
+    nom.innerText = tab_Personne[numeroPersonne][0];
+    nom.classList.add("nom")
+
+    emplacement_modal.firstElementChild.appendChild(nom);
+
+    for (let i = 1; i < tab_Personne[numeroPersonne].length; i++) {
 
         let element = document.createElement("div");
         element.innerText = tab_Personne[numeroPersonne][i];
